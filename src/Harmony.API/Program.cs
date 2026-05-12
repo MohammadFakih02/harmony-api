c444d448a31aae4b0eeec39c250e1b50795e989b
Mohammad Fakih <mohammadfakih025@gmail.com>
2026-05-12 13:26:14 +0300

    feat: add Snowflake ID generator service (#1)

--- src/Harmony.API/Program.cs
+++ src/Harmony.API/Program.cs
@@ -1,7 +1,14 @@
+using Harmony.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var workerId = builder.Configuration.GetValue<long>("Snowflake:WorkerId", 0);
+var datacenterId = builder.Configuration.GetValue<long>("Snowflake:DatacenterId", 0);
+
+builder.Services.AddSingleton<ISnowflakeIdGenerator>(
+    _ => new SnowflakeIdGenerator(workerId, datacenterId));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,28 +21,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
