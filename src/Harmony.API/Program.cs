110f3fb7feea238f813846fcf1e5213ea913b057
Mohammad Fakih <mohammadfakih025@gmail.com>
2026-05-13 13:04:44 +0300

    Feature/asp net identity setup (#3)
    * fix:  restore identity columns so users can be created
    
    * feat: configure asp-net identity with custom user entity

--- src/Harmony.API/Program.cs
+++ src/Harmony.API/Program.cs
@@ -1,18 +1,39 @@
+using Harmony.Core.Domain.Entities;
 using Harmony.Core.Services;
 using Harmony.Infrastructure.Postgres;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 var workerId = builder.Configuration.GetValue<long>("Snowflake:WorkerId", 0);
 var datacenterId = builder.Configuration.GetValue<long>("Snowflake:DatacenterId", 0);
 
-builder.Services.AddSingleton<ISnowflakeIdGenerator>(
-    _ => new SnowflakeIdGenerator(workerId, datacenterId));
+builder.Services.AddSingleton<ISnowflakeIdGenerator>(_ => new SnowflakeIdGenerator(
+    workerId,
+    datacenterId
+));
 
 builder.Services.AddDbContext<HarmonyDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+);
+
+builder.Services.AddDataProtection();
+
+builder
+    .Services.AddIdentityCore<User>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<HarmonyDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
@@ -25,6 +46,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
-
-
+app.Run();
